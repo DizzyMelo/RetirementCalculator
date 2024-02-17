1cde7a8e2152b53a89dfa86d1e5484dcdff43883
Daniel Melo <daniel.melo42@outlook.com>
2024-02-17 05:38:04 -0600

    track events

--- app/src/main/java/com/dicedev/retirementcalculator/MainActivity.kt
+++ app/src/main/java/com/dicedev/retirementcalculator/MainActivity.kt
@@ -2,14 +2,19 @@ package com.dicedev.retirementcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dicedev.retirementcalculator.databinding.ActivityMainBinding
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         AppCenter.start(
             application,
@@ -17,5 +22,29 @@ class MainActivity : AppCompatActivity() {
             Analytics::class.java,
             Crashes::class.java
         )
+
+        binding.calculateButton.setOnClickListener {
+//            Crashes.generateTestCrash()
+            try {
+                val interestRate = binding.interestEditText.text.toString().toFloat()
+                val age = binding.ageEditText.text.toString().toInt()
+                val retirementAge = binding.retirementEditText.text.toString().toInt()
+                val monthlySavings = binding.monthlySavingsEditText.text.toString().toInt()
+                val currentSavings = binding.currentEditText.text.toString().toInt()
+
+                val hashMap = hashMapOf<String, String>()
+
+                if (interestRate <= 0) {
+                    Analytics.trackEvent("wrong_interest_rate")
+                }
+
+                if (retirementAge <= age) {
+                    Analytics.trackEvent("wrong_retirement_age")
+                }
+            } catch (ex: Exception) {
+                Analytics.trackEvent(ex.message)
+            }
+        }
+
     }
 }
